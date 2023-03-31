cead92dfeba20e9840d5df332107d68cbf162378
Vini7k <vinirangel58@gmail.com>
2023-03-31 17:43:18 -0300

    Le e imprime as idades

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,33 +1,21 @@
-﻿    // See https://aka.ms/new-console-template for more information
+﻿//EXERCICIOS COM VETORES
 
-Console.WriteLine("Hello, World!");
-string nome = "Vinicus ";
-string nome1 = "Lele";
-string nome2 = "Loeza ";
-Console.WriteLine("Seja bem-vindo " + nome);
-string carro = "Kadett";
-Console.WriteLine($"Eu gosto de {carro} ");
+/*
+ * crie um vetor para armazenar as idades de 5 pessoas e ao final imprima:
+ * 1) a pessoa mais nova
+ * 2) a pessoa mais velha
+ * 3)a média das idades
+ * 4) todas as idades impares
+ * 5) todas as idades pares 
+ */
+byte[] idades = new byte[5];
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Informe a {i + 1} idade:");
+    idades[i] = byte.Parse(Console.ReadLine());
+}
 
-string sobrenome = "Rangel";
-string sobrenome2 = "Cardoso";
-Console.WriteLine($"Estes são {nome + sobrenome} e {nome2 + sobrenome2}");
-
-byte valor = 255;
-valor += 1; // valor = valor + 1;
-Console.WriteLine($"valor: {valor}");
-
-decimal salario = 5000.20m;
-Console.WriteLine(salario);
-
-string nome3 = "Leticia e Vinicus";
-Console.WriteLine("Ultimos românticos: "+ nome3);
-
-Console.WriteLine($"{nome} e {nome1} juntos são tcp");
-
-float altura = 1.76f;
-double altura2 = altura * 0.000000001;
-float peso = 83.2f; 
-Console.WriteLine($"A altura é: {altura} m");
-Console.WriteLine($"O peso é: {peso}kg");
-Console.WriteLine($"Se vc pudesse ser o homem formiga, teria a altura {altura2}nm");
-
+foreach (byte idade in idades)
+{
+    Console.WriteLine(idade);
+}
