f15baa59ca57ddae72aa9ed003d1c580e5fe6c31
Viviane180805 <vivianegm18@gmail.com>
2023-04-13 15:57:44 -0300

    aula1304

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -8,15 +8,14 @@
  * 4) todas as idades impares
  * 5) todas as idades pares 
  */
+
 byte[] idades = new byte[5];
 byte menorIdade, maiorIdade;
 float media;
 int par, impar, somador;
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine($"Informe a {i + 1} idade:");
-    idades[i] = byte.Parse(Console.ReadLine());
-}
+
+
+EntradaDeDados();
 menorIdade = idades[0];
 maiorIdade = idades[0];
 par = idades[0];
@@ -48,15 +47,36 @@ Console.WriteLine($"Maior idade:{maiorIdade}");
 media = (idades[0] + idades[1] + idades[2] + idades[3] + idades[4]) / 5;
 Console.WriteLine($"a media é {media}");
 
+Console.WriteLine("as idades pares são:");
+
 foreach (byte idade in idades)
 {
     if (idade % 2 == 0) 
     {
-        Console.WriteLine($"as idades par são:{par}");
+        par = idade;
+        Console.WriteLine(par);
     }
+    
+} 
+Console.WriteLine("as idades impares são:");
+foreach (byte idade in idades)
+{
+    if (idade % 2 != 0) 
     {
-
+        impar = idade;
+        Console.WriteLine(impar);
     }
-       
+    
 }
+
+void EntradaDeDados()
+{
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Informe a {i + 1} idade:");
+    idades[i] = byte.Parse(Console.ReadLine());
+}
+}
+
+
 
